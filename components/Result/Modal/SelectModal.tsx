2a12bc2b2f3bf66401818099a92de4f39064de5d
이원학 <2wonakk@gmail.com>
2021-10-31 16:04:08 +0900

    feat(select): column and function 추가 기능 구현

--- components/Result/Modal/SelectModal.tsx
+++ components/Result/Modal/SelectModal.tsx
@@ -1,15 +1,14 @@
 import ModalContext from "context/modal-context";
 import QueryContext from "context/query-context";
-import StoreContext from "context/store-context";
-import React, { useContext, useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { HTMLProps, useContext, useEffect, useState } from "react";
+
 import { Modal } from "./Modal";
 import Autocomplete from "react-autocomplete";
 import { matchColumn } from "utils";
 import ResultContext from "context/result-context";
 
 interface ISelectModalProps {}
-const funcRecommendations = ["MAX", "MIN"];
+const funcRecommendations = ["MAX", "MIN", "UNIQUE"];
 export const SelectModal: React.FC<ISelectModalProps> = (props) => {
   const { selectModal } = useContext(ModalContext);
   const { recommendations } = useContext(ResultContext);
@@ -17,6 +16,16 @@ export const SelectModal: React.FC<ISelectModalProps> = (props) => {
   const [select, setSelect] = useState<string>("");
   const [func, setFunc] = useState<string>("");
 
+  const onSubmit = () => {
+    query.select.push({ column: select, agg: func.toUpperCase() });
+    setQuery({ ...query, select: query.select });
+    selectModal.setVisible(false);
+  };
+
+  const onError = (e) => {
+    // e.column.required
+    console.log(e);
+  };
   return (
     <Modal
       onClose={() => {
@@ -25,91 +34,94 @@ export const SelectModal: React.FC<ISelectModalProps> = (props) => {
       maskClosable={true}
       closable={true}
       visible={selectModal.visible}>
-      <div className="p-4 h-40 shadow">
+      <div className="p-4 shadow">
+        <div className="pb-1">
+          <SearchInput
+            className="pb-1"
+            placeholder="column"
+            recommendations={recommendations}
+            onSelect={(value) => setSelect(value)}
+          />
+        </div>
         <SearchInput
-          recommendations={recommendations}
-          onSelect={(value) => setSelect(value)}
-        />
-        <SearchInput
+          placeholder="function"
           recommendations={funcRecommendations}
           onSelect={(value) => setFunc(value)}
         />
-        <button
-          onClick={(e) => {
-            query.select.push({ column: select, agg: func.toUpperCase() });
-            setQuery({ ...query, select: query.select });
-          }}>
-          Search
-        </button>
+        {/* buttons */}
+        <div className="flex justify-between items-center mt-8">
+          <button
+            type="button"
+            className="border border-gray-900 p-1 flex-grow mr-2"
+            onClick={onSubmit}>
+            Search
+          </button>
+          <button
+            type="button"
+            className="border border-gray-900 p-1 flex-grow"
+            onClick={(e) => {
+              selectModal.setVisible(false);
+            }}>
+            Cancel
+          </button>
+        </div>
       </div>
     </Modal>
   );
 };
 
-interface ISearchInputProps {
+interface ISearchInputProps extends HTMLProps<HTMLInputElement> {
   recommendations: string[];
   onSelect: (value: any) => void;
 }
 
-const SearchInput: React.FC<ISearchInputProps> = ({ children, ...props }) => {
-  const [input, setInput] = useState("");
+const SearchInput: React.FC<ISearchInputProps> = React.forwardRef(
+  ({ children, recommendations, onSelect, ...props }, ref) => {
+    const [input, setInput] = useState("");
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    setValue,
-    formState: { errors },
-  } = useForm();
+    useEffect(() => {
+      onSelect(input);
+    }, [input]);
 
-  useEffect(() => {
-    props.onSelect(input);
-  }, [input]);
-
-  return (
-    // {/* input */}
-    <div className="flex border border-gray-500 rounded w-full bg-white">
-      <Autocomplete
-        {...register("query", {
-          validate: (value) => value !== "",
-        })}
-        value={input}
-        inputProps={{
-          id: "states-autocomplete",
-          className: "focus:outline-none p-1 rounded-l w-full",
-        }}
-        wrapperStyle={{
-          position: "relative",
-          display: "inline-block",
-          flexGrow: 1,
-          zIndex: 1,
-        }}
-        items={props.recommendations}
-        getItemValue={(item: any) => item}
-        shouldItemRender={matchColumn}
-        // sortItems={sortStates}
-        onChange={(event, value) => setInput(value)}
-        onSelect={setInput}
-        renderMenu={(children) => (
-          <div className="absolute box-border border border-gray-300 bg-indigo-50">
-            {children}
-          </div>
-        )}
-        renderItem={(item: any, isHighlighted: boolean) => (
-          <div
-            className={`p-1 cursor-pointer ${
-              isHighlighted ? "text-white bg-blue-600" : ""
-            }`}
-            key={item}>
-            {item}
-          </div>
-        )}
-      />
-      <button
-        className="bg-blue-600 text-white m-0.5 pr-1 pl-1 rounded"
-        onClick={() => props.onSelect(input)}>
-        Enter
-      </button>
-    </div>
-  );
-};
+    return (
+      // {/* input */}
+      <div className="flex border border-gray-500 w-full bg-white">
+        <Autocomplete
+          ref={ref}
+          value={input}
+          inputProps={{
+            ...props,
+            id: "states-autocomplete",
+            className: "focus:outline-none p-1 w-full",
+          }}
+          wrapperStyle={{
+            // position: "relative",
+            display: "inline-block",
+            flexGrow: 1,
+            // zIndex: 1,
+          }}
+          items={recommendations}
+          getItemValue={(item: any) => item}
+          shouldItemRender={matchColumn}
+          // sortItems={sortStates}
+          onChange={(event, value) => setInput(value)}
+          onSelect={setInput}
+          renderMenu={(children) => (
+            <div className="absolute box-border border border-gray-300 bg-indigo-50">
+              {children}
+            </div>
+          )}
+          renderItem={(item: any, isHighlighted: boolean) => (
+            <div
+              className={`p-1 cursor-pointer ${
+                isHighlighted ? "text-white bg-blue-600" : ""
+              }`}
+              key={item}>
+              {item}
+            </div>
+          )}
+        />
+      </div>
+    );
+  }
+);
